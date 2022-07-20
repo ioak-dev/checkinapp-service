c8f5ac387ae5d046b08fff11f9e0e3589ccf9fd5
Arun Kumar Selvaraj <arun@ioak.io>
2022-07-20 13:02:45 +0530

    Enhance participant domai gitpush

--- src/modules/participant/model.ts
+++ src/modules/participant/model.ts
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const participantSchema = new Schema(
   {
-    participantId: { type: String },
+    referenceId: { type: String },
     firstName: { type: String },
     lastName: { type: String },
     eventId: { type: String },
